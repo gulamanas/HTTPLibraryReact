19a6dfee9dce18b93faa2b8611fe466209b01283
Gulam Anas <g.anas786@gmail.com>
2021-12-17 21:24:22 +0530

    post request

---
+++ src/components/PostForm.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+class PostForm extends Component {
+    render() {
+        return (
+            <div>
+                <form>
+                    <div>
+                        <input type="text" name="userId" />
+                    </div>
+                    <div>
+                        <input type="text" name="title" />
+                    </div>
+                    <div>
+                        <input type="text" name="body" />
+                    </div>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default PostForm
